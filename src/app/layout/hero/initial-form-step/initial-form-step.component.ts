490cff0eb22deaac3ab89ea85f64261613fb6a71
oostrobin <153209318+oostrobin@users.noreply.github.com>
2024-01-18 17:08:39 +0100

    Update styles and HTML templates

--- src/app/layout/hero/initial-form-step/initial-form-step.component.ts
+++ src/app/layout/hero/initial-form-step/initial-form-step.component.ts
@@ -13,6 +13,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormStateService } from '../../../shared/services/form-state/form-state.service';
 import { Subject, takeUntil } from 'rxjs';
 import { FORM_CONFIG, FormConfig } from './config/form-config.constant';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'form-step-one',
@@ -23,6 +24,7 @@ import { FORM_CONFIG, FormConfig } from './config/form-config.constant';
     ReactiveFormsModule,
     MatInputModule,
     MatCardModule,
+    MatIconModule
   ],
   templateUrl: './initial-form-step.component.html',
   styleUrl: './initial-form-step.component.scss',
